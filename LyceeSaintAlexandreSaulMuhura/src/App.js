dc8df2b267c8583ad0ba9ac4e795e35c43be222d
Deodate <ajadeo04@gmail.com>
2024-11-04 17:53:02 +0200

    Progress

---
+++ LyceeSaintAlexandreSaulMuhura/src/App.js
@@ -0,0 +1,77 @@
+import { BrowserRouter, Route, Routes } from "react-router-dom"
+import { Layout } from "./components/common/Layout"
+import { Home } from "./pages/Home"
+import { BlogSinglePage } from "./components/common/BlogSinglePage"
+import { About } from "./pages/About"
+import { Courses } from "./pages/WhyLycee"
+import { Blog } from "./pages/NewsEvent"
+import { Instructor } from "./pages/ContactUs"
+
+function App() {
+  return (
+    <>
+      <BrowserRouter>
+        <Routes>
+          <Route
+            path='/'
+            element={
+              <Layout>
+                <Home />
+              </Layout>
+            }
+          />
+          <Route
+            path='/about'
+            element={
+              <Layout>
+                <About />
+              </Layout>
+            }
+          />
+          <Route
+            path='/courses'
+            element={
+              <Layout>
+                <Courses />
+              </Layout>
+            }
+          />
+          <Route
+            path='/instructor'
+            element={
+              <Layout>
+                <Instructor />
+              </Layout>
+            }
+          />
+           <Route
+            path='/staff'
+            element={
+              <Layout>
+                <staff />
+              </Layout>
+            }
+          />
+          <Route
+            path='/blog'
+            element={
+              <Layout>
+                <Blog />
+              </Layout>
+            }
+          />
+          <Route
+            path='/single-blog'
+            element={
+              <Layout>
+                <BlogSinglePage />
+              </Layout>
+            }
+          />
+        </Routes>
+      </BrowserRouter>
+    </>
+  )
+}
+
+export default App
---
+++ LyceeSaintAlexandreSaulMuhura/src/pages/Home.jsx
@@ -0,0 +1,91 @@
+import React from "react"
+import heroImg from "../components/assets/images/hero.png"
+// import heroImgback from "../components/assets/images/hero-shape-purple.png"
+import { BsFillLightningChargeFill } from "react-icons/bs"
+import { FaBookReader, FaGraduationCap, FaUsers } from "react-icons/fa"
+import { About } from "./About"
+import { Courses } from "./WhyLycee"
+import { Instructor } from "./ContactUs"
+import { Blog } from "./NewsEvent"
+
+
+export const Home = () => {
+  return (
+    <>
+      <HomeContent />
+      <About />
+      <br />
+      <br />
+      <br />
+      <Courses />
+      <Instructor />
+      <Blog />
+    </>
+  )
+}
+export const HomeContent = () => {
+  return (
+    <>
+      <section className='bg-secondary py-10 h-[92vh] md:h-full'>
+        <div className='container'>
+          <div className='flex items-center justify-center md:flex-col'>
+            <div className='left w-1/2 text-black md:w-full'>
+              <h1 className='text-4xl leading-tight text-black font-semibold'>
+                Welcome To<br /> Lycée Saint Alexandre Sauli de Muhura <br />
+              </h1>
+              <h3 className='text-lg mt-10'>INNOVATE TOGETHER FOR  SUCCESS</h3>
+            </div>
+            <div className='right w-1/2 md:w-full relative'>
+              <div className='images relative'>
+                {/* <img src={heroImgback} alt='' className=' absolute top-32 left-10 w-96 md:left-10' /> */}
+                <div className='img h-[95vh] w-full'>
+                  <img src={heroImg} alt='' className='h-[95%] w-[93%] object-contain z-20 relative rounded-lg' />
+                </div>
+
+              </div>
+              <div className='content'>
+                <button className='bg-white shadow-md absolute top-56 left-0 z-30 p-2 flex items-center rounded-md'>
+                  <div className='icon w-10 h-10 text-white rounded-full flex items-center justify-center bg-orange-400'>
+                    <BsFillLightningChargeFill size={19} />
+
+                  </div>
+                  <div className='text flex flex-col items-start px-4'>
+                    <span className='text-sm text-black'>Computer System and Architecture </span>
+                    <span className='text-[12px]'>Your admission completed</span>
+                  </div>
+                </button>
+                <button className='bg-white shadow-md absolute bottom-32 left-48 z-30 p-2 flex items-center rounded-md pr-8'>
+                  <div className='icon w-10 h-10 text-white rounded-full flex items-center justify-center bg-blue-400'>
+                    <FaGraduationCap size={20} />
+                  </div>
+                  <div className='text flex flex-col items-start px-2'>
+                    <span className='text-sm text-black'>Network and Internet Technology</span>
+
+                  </div>
+                </button>
+                <button className='bg-white shadow-md absolute top-56 -right-32 z-30 p-2  md:top-96 md:-right-5 flex items-center rounded-md'>
+                  <div className='icon w-10 h-10 text-white rounded-full flex items-center justify-center bg-orange-400'>
+                    <FaUsers size={23} />
+                  </div>
+                  <div className='text flex flex-col items-start px-0'>
+                    <span className='text-sm text-black'> Software Development</span>
+                    {/* <span className='text-[12px]'>Tomorrow is our</span> */}
+                  </div>
+                </button>
+                <button className='bg-white shadow-md absolute top-32 right-32 z-30 p-2 flex items-center rounded-md'>
+                  <div className='icon w-10 h-10 text-white rounded-full flex items-center justify-center bg-indigo-400'>
+                    <FaBookReader size={20} />
+                  </div>
+                  <div className='text flex flex-col items-start px-2'>
+                    <span className='text-sm text-black'>Computer System and Architecture</span>
+
+                  </div>
+                </button>
+              </div>
+            </div>
+          </div>
+        </div>
+      </section>
+    </>
+  )
+}
